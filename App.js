75fd23eb273a8114290c71aa278daefcb357a621
Niv Sardi <xaiki@evilgiggle.com>
2018-06-20 05:04:24 -0300

    init
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native';
+
+export default class Radar extends React.Component {
+  render() {
+    return (
+      <WebView
+        source={{uri: 'https://radar.redelivre.org/'}}
+        style={{marginTop: 20}}
+      />
+    );
+  }
+}
