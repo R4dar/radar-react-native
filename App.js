0d75d5993e8d67b70354923c4d2e4942e228cf76
Niv Sardi <xaiki@evilgiggle.com>
2018-06-27 11:56:35 -0300

    get expo to actually ask for permisions
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- App.js
+++ App.js
@@ -2,8 +2,26 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native';
 
+import Expo from 'expo';
+
+const { Location, Permissions } = Expo;
+
 export default class Radar extends React.Component {
   render() {
+
+    const getPermisions = async () => {
+      await Promise.all([
+        Permissions.NOTIFICATIONS,
+        Permissions.USER_FACING_NOTIFICATIONS,
+        Permissions.LOCATION,
+        Permissions.CAMERA,
+        Permissions.AUDIO_RECORDING,
+        Permissions.CAMERA_ROLL
+      ].map(permision => Permissions.askAsync(permision)))
+    }
+
+    getPermisions()
+
     return (
       <WebView
         source={{uri: 'https://radar.redelivre.org/'}}
